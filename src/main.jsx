6e38acc8a6dd2d491e9a1f6d43c7d7d829694fd9
nikolaszea <nikolas.michael.zea@gmail.com>
2025-03-08 15:37:02 -0500

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import LandingPage from "./LandingPage";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <LandingPage />
-  </React.StrictMode>
-);
