e1ee47e46a13f16bcfec9a75a0fc619ae584a65f
nikolaszea <nikolas.michael.zea@gmail.com>
2025-03-08 17:58:32 -0500

    Fix TypeScript errors in ModeSelector component

---
+++ src/appp.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to PAL-Imperator</h1>
+        <p>
+          This is a basic template for your React application using TypeScript.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
